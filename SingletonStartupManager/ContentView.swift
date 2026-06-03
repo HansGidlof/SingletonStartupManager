7bed0cf091b674aacf6e9532614950d192b4a228
Hans Gidlof <hansgidlof@icloud.com>
2026-06-03 13:27:18 +0200

    Initial Commit

---
+++ SingletonStartupManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SingletonStartupManager
+//
+//  Created by Hans Gidlof on 2026-06-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SingletonStartupManager/SingletonStartupManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingletonStartupManagerApp.swift
+//  SingletonStartupManager
+//
+//  Created by Hans Gidlof on 2026-06-03.
+//
+
+import SwiftUI
+
+@main
+struct SingletonStartupManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
